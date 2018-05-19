67abd7cbdee63b1dce6d09ebb7e334f85f85a120
Alexander Glover <alexander.e.glover@gmail.com>
2018-05-19 15:44:17 -0400

    FontUpload&StyleChange
    Upload Montserrat Font
    Change to white page, black background

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,7 +1,7 @@
 function setHovered(ele){
-  ele.style.color = "#3DDCD6";
+  ele.style.color = "#00205B";
 }
 
 function setUnHovered(ele){
-  ele.style.color = "#FFFFFF";
+  ele.style.color = "#000000";
 }
